0efed3a58dea1fa668950cb3e3640273f8456fe1
66Meg20 <66meg.dj2066@gmail.com>
2023-05-30 23:38:18 -0500

    added content to the page and a css file

--- src/Books.js
+++ src/Books.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-
+import "./Books.css";
 export default function Books() {
+  const [bookData, setBookData] = useState({ ready: false });
   function handleResponse(response) {
     console.log(response.data);
-  }
 
-  let apiUrl = `https://openlibrary.org/search.json?title=the+lord+of+the+rings`;
-  axios.get(apiUrl).then(handleResponse);
+    setBookData({
+      ready: true,
+      title: response.data.docs[0].title,
+    });
+  }
+  if (bookData.ready) {
+    return (
+      <div className="Books">
+        <h1> {bookData.title}</h1>
+        <form>
+          <input type="search" placeholder="book name" />
+          <button>Search</button>
+        </form>
+        <ul>
+          <li>From blood and ash</li>
+          <li>Throne of glass</li>
+          <li>A court of thorns and roses</li>
+        </ul>
+      </div>
+    );
+  } else {
+    let apiUrl = `https://openlibrary.org/search.json?title=from+blood+and+ash`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
